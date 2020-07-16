eefa177b81e8792907312b086a852c6821734a1a
kevy7 <kvnyang503@gmail.com>
2020-07-15 20:08:21 -0700

    imported and added container component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Container from './Components/Container/Container';
 
 function App() {
   return (
     <div className="App">
       {/* Components will be imported below */}
+      {/* Navbar within a container */}
+      <Container />
     </div>
   );
 }
