1a59cf13230abb9820133387ce36dc5a8519f177
kevy7 <kvnyang503@gmail.com>
2020-07-15 20:31:35 -0700

    updated styling for containers

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styles from './Container.module.css';
+
+//How to use css modules
+//return <button className={styles.error}>Error Button</button>;
 
 const Container = (props) => {
     return (
-        <div className="container">
+        <div className={styles.container}>
             
         </div>
     )
