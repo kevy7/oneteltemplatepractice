4a08cf4c9946f8faf57ade6366863f03de66ee7e
kevy7 <kvnyang503@gmail.com>
2020-07-16 18:39:11 -0700

    created classname and new list element for navbar

--- src/Components/Container/Navbar/Navbar.js
+++ src/Components/Container/Navbar/Navbar.js
@@ -7,6 +7,12 @@ const NavBar = (props) => {
             {/* left side should contain navbar */}
 
             {/* right side should contain the menu items */}
+            <ul className={styles.menuList}>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
         </div>
     )
 }
