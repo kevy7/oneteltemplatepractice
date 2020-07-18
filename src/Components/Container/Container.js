1c9546552c5773b837630aad587ae6e07e698f4b
kevy7 <kvnyang503@gmail.com>
2020-07-17 21:48:51 -0700

    removed comments

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -2,9 +2,6 @@ import React from 'react';
 import styles from './Container.module.css';
 import Navbar from './Navbar/Navbar';
 
-//How to use css modules
-//return <button className={styles.error}>Error Button</button>;
-
 const Container = (props) => {
     return (
         <div className={styles.container}>
