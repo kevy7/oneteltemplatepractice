c9b6e0bfc4bf2977b3ac420a4fc5f7641a0f03a4
kevy7 <kvnyang503@gmail.com>
2020-07-18 15:54:23 -0700

    imported image and changed name of image

--- src/Components/Container/TopComponent/TopComponent.js
+++ src/Components/Container/TopComponent/TopComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from "./TopComponent.module.css";
+import backgroundImg from '../../../Images/';
 
 const TopComponent = () => {
     return (
         <div className={styles.topComponent}>
-            
+
         </div>
     );
 }
