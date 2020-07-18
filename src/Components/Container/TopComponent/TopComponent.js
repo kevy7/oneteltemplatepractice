d9f055603c9f02344abfafbc815ce88e03e56c35
kevy7 <kvnyang503@gmail.com>
2020-07-18 15:57:41 -0700

    no need for background images

--- src/Components/Container/TopComponent/TopComponent.js
+++ src/Components/Container/TopComponent/TopComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./TopComponent.module.css";
-import backgroundImg from '../../../Images/';
+//import backgroundImg from '../../../Images/bgImage.jpg';
 
 const TopComponent = () => {
     return (
