6263a4abc51d48bfdb29b854c770a7e9f164efdb
kevy7 <kvnyang503@gmail.com>
2020-07-18 15:31:58 -0700

    updated Container js file

--- src/Components/Container/TopComponent/TopComponent.js
+++ src/Components/Container/TopComponent/TopComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from "./TopComponent.module.css";
 
 const TopComponent = () => {
     return (
-        <div>
-            
+        <div className={}>
+
         </div>
     );
 }
