14b64a3b2d537aa23df841c518e38261bc0e4c0e
kevy7 <kvnyang503@gmail.com>
2020-07-18 15:36:27 -0700

    removed TopComponent from container js file

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './Container.module.css';
 import Navbar from './Navbar/Navbar';
-import TopComponent from './TopComponent/TopComponent';
 
 const Container = (props) => {
     return (
         <div className={styles.container}>
             <Navbar />
-            <TopComponent />
-            
         </div>
     )
 }
--- src/Components/Container/TopComponent/TopComponent.js
+++ src/Components/Container/TopComponent/TopComponent.js
@@ -3,8 +3,8 @@ import styles from "./TopComponent.module.css";
 
 const TopComponent = () => {
     return (
-        <div className={}>
-
+        <div className={styles.topComponent}>
+            
         </div>
     );
 }
