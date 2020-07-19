46e294837eb86a6c80b48e244cd03aeb02040f7f
kevy7 <kvnyang503@gmail.com>
2020-07-19 12:52:12 -0700

    updated the middleComponent

--- src/Components/MiddleComponent/MiddleComponent.js
+++ src/Components/MiddleComponent/MiddleComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './MiddleComponent.module.css';
+import MCompContainer from './mCompContainer/mCompContainer';
 
 const MiddleComponent = () => {
     return (
         <div className={styles.middleComponent}>
-
+            <MCompContainer />
         </div>
     )
 }
