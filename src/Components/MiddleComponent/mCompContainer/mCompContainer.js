19d6bc33bd88b78fc8dea75fb378c510777eedb0
kevy7 <kvnyang503@gmail.com>
2020-07-20 18:39:58 -0700

    imported block element into the mCompContainer

--- src/Components/MiddleComponent/mCompContainer/mCompContainer.js
+++ src/Components/MiddleComponent/mCompContainer/mCompContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './mCompContainer.module.css';
+import BlockElement from './BlockElement/BlockElement';
 
 const MCompContainer = () => {
     return (
