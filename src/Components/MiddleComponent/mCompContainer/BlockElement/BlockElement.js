89c4221903c08d92047ffaf62e4a813a08496649
kevy7 <kvnyang503@gmail.com>
2020-07-23 18:35:05 -0700

    added propTypes in the BlockElement

--- src/Components/MiddleComponent/mCompContainer/BlockElement/BlockElement.js
+++ src/Components/MiddleComponent/mCompContainer/BlockElement/BlockElement.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './BlockElement.module.css';
+import PropTypes from 'prop-types';
 
 const BlockElement = () => {
     return (
         <div className={styles.blockElement}>
-            
+
         </div>
     );
 }
+
+/* BlockElement.propTypes {
+
+} */
 
 export default BlockElement;
