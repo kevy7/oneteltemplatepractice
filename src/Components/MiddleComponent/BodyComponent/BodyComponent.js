ff29be13510a6c0e74f87920616908e906568f9f
kevy7 <kvnyang503@gmail.com>
2020-07-26 19:54:55 -0700

    updated styling

--- src/Components/MiddleComponent/BodyComponent/BodyComponent.js
+++ src/Components/MiddleComponent/BodyComponent/BodyComponent.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import styles from './BodyComponent/module.css';
+import styles from './BodyComponent.module.css';
 
 const BodyComponent = () => {
+    return (
+        <div className={styles.bodyComponent}>
 
+        </div>
+    )
 }
 
 export default BodyComponent;
--- src/Components/MiddleComponent/MiddleComponent.js
+++ src/Components/MiddleComponent/MiddleComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './MiddleComponent.module.css';
 import MCompContainer from './mCompContainer/mCompContainer';
+import BodyComponent from './BodyComponent/BodyComponent';
 
 const MiddleComponent = () => {
     return (
         <div className={styles.middleComponent}>
             <MCompContainer />
             {/* add Body container here */}
+            <BodyComponent />
         </div>
     )
 }
