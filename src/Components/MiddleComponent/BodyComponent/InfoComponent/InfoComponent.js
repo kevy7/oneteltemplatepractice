97262c16da9aad3ae9d1a9ec174b2d8b60066c6a
kevy7 <kvnyang503@gmail.com>
2020-07-28 20:19:45 -0700

    made updates to component

--- src/Components/MiddleComponent/BodyComponent/InfoComponent/InfoComponent.js
+++ src/Components/MiddleComponent/BodyComponent/InfoComponent/InfoComponent.js
@@ -4,11 +4,9 @@ import styles from './InfoComponent.module.css';
 const infoComponent = () => {
     return (
         <div className={styles.infoComponent}>
-            <div className={styles.imgBackground}>
-
-            </div>
+            <div className={styles.imgBackground}></div>
             <div className={styles.infoDescription}>
-
+                <h1></h1>
             </div>
         </div>
     )
