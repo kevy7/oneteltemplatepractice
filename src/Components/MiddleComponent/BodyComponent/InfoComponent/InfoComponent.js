7881ed992d227294ff0cf367f5efbf4d65f2fd54
kevy7 <kvnyang503@gmail.com>
2020-07-29 23:20:07 -0700

    added classnames to paragraphs

--- src/Components/MiddleComponent/BodyComponent/InfoComponent/InfoComponent.js
+++ src/Components/MiddleComponent/BodyComponent/InfoComponent/InfoComponent.js
@@ -7,8 +7,8 @@ const infoComponent = () => {
             <div className={styles.imgBackground}></div>
             <div className={styles.infoDescription}>
                 <h1 className={styles.title}>Title Here</h1>
-                <p>This is a random paragraph. sdngkjsdn d fdsnfklds fnds lfndsf lsd flsdknf dslknf ldsn flds fds fn lkn fsdlnf sdkn f</p>
-                <p>
+                <p className={styles.infoParagraph}>This is a random paragraph. sdngkjsdn d fdsnfklds fnds lfndsf lsd flsdknf dslknf ldsn flds fds fn lkn fsdlnf sdkn f</p>
+                <p className={styles.infoParagraph}>
                     tjisdf ks djlsnfld fldsnflds flds fdknf dfl dlfmn dslkn flsd flknlfkn dlf l flkdn flkns fldsn flds fld  dslfn lfndsf
                     sdnfjlnfldf  fldks fds flkdn flnds fldkn flns flnd fskldn fnflknmdsklf eij rw dsbfuhewi jw guhbw eofhowhf oshf sf jf
                     sfhu eoh owihg orhfgoh wfh j kj giewj uwhfo hejh ighoije pjfpiwfoi sdfno.
