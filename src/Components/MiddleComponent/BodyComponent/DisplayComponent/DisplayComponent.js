89b748ded2f8f1fb8630031bfb359744abe34b06
kevy7 <kvnyang503@gmail.com>
2020-07-30 21:59:15 -0700

    removed display Info Comp

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './DisplayComponent.module.css';
 
+
 const DisplayComponent = () => {
     return (
         <div className={styles.displayComponent}>
             
+            <div></div>
         </div>
     )
 }
---
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayInfoComp/DisplayInfoComp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Styles from './DisplayInfoComp.module.css';
+
+const DisplayInfoComp = () => {
+    return (
+        <div className={Styles.displayInfoComp}>
+            <h1>Title</h1>
+            <p>
+                sjsio sdhf dhsfdihs fdi flidhs gdhnl  sdfid lf hdslif hdsilh fldskng ldsng
+                njkdf s bnl jg sldkjn djgn n fglkfg fsglkfsng lknsjg lkfn gfg lkfn g sdfgjl glf 
+                fnlgn lf fs gnlksfg lfijng s glkfns glfsng lfkm gkfsn jglis gls gsjgn fgsngjl.
+            </p>
+        </div>
+    )
+}
+
+export default DisplayInfoComp;
