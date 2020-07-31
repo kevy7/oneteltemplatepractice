a24d59fcf57fe2068f400b991ae669ff625632ea
kevy7 <kvnyang503@gmail.com>
2020-07-31 16:10:31 -0700

    imported display info component

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './DisplayComponent.module.css';
-
+import DisplayInfoComp from './DisplayInfoComp/DisplayInfoComp';
 
 const DisplayComponent = () => {
     return (
         <div className={styles.displayComponent}>
+            <h1>Title</h1>
             
         </div>
     )
