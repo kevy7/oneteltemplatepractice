aa9fda382d0ceab0e984e78abadde10e634036d1
kevy7 <kvnyang503@gmail.com>
2020-08-01 10:08:42 -0700

    imported gallery component to displaycompoennt

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './DisplayComponent.module.css';
 import DisplayInfoComp from './DisplayInfoComp/DisplayInfoComp';
+import GalleryComponent from './GalleryComponent/GalleryComponent';
 
 const DisplayComponent = () => {
     return (
         <div className={styles.displayComponent}>
             <DisplayInfoComp />
-            
+            <GalleryComponent />
         </div>
     )
 }
