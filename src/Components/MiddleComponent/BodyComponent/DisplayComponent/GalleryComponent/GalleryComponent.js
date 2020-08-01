ddb7230a1271d2e78dda4c0880033eb70099098d
kevy7 <kvnyang503@gmail.com>
2020-08-01 10:07:24 -0700

    added boostrap components to the GalleryComponent

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -5,7 +5,18 @@ import { Container, Row, Col} from 'react-bootstrap';
 const GalleryComponent = () => {
     return (
         <div className={styles.galleryComponent}>
-
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={6}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={5}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
         </div>
     )
 }
