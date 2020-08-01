5d54b0d13c1ac35b4ac4b080de6a9096210ed9d9
kevy7 <kvnyang503@gmail.com>
2020-08-01 08:45:39 -0700

    imported css module styles

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './GalleryComponent.module.css';
 
 const GalleryComponent = () => {
-
+    return (
+        <div className={styles.galleryComponent}>
+            
+        </div>
+    )
 }
 
 export default GalleryComponent;
