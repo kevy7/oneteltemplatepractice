fd4c42a31ba8aa323676c939214c5edc0b1e1a65
kevy7 <kvnyang503@gmail.com>
2020-08-01 09:46:39 -0700

    updated and added comments

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './GalleryComponent.module.css';
 
+
+//Learning how to create a boostrap grid system with react
+//https://www.youtube.com/watch?v=RaFN1KU6cDU
+
 const GalleryComponent = () => {
     return (
         <div className={styles.galleryComponent}>
-            
+
         </div>
     )
 }
