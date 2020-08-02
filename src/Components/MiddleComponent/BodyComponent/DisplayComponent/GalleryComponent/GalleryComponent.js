cda19dd582f5c82bd2fadb8ac503c3b3e8235045
kevy7 <kvnyang503@gmail.com>
2020-08-02 10:50:40 -0700

    changed position for import

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import styles from './GalleryComponent.module.css';
 import { Container, Row, Col} from 'react-bootstrap';
+import styles from './GalleryComponent.module.css';
+//import { Container, Row, Col} from 'react-bootstrap';
 
 const GalleryComponent = () => {
     return (
