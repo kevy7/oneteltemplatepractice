425536df31633fa055078486553e51ce83058e4d
kevy7 <kvnyang503@gmail.com>
2020-08-02 10:43:23 -0700

    updated Gallery Component

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -7,16 +7,15 @@ const GalleryComponent = () => {
         <div className={styles.galleryComponent}>
             <Container>
                 <Row>
-                    <Col>1 of 3</Col>
-                    <Col xs={6}>2 of 3 (wider)</Col>
-                    <Col>3 of 3</Col>
+                    <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
                 </Row>
                 <Row>
                     <Col>1 of 3</Col>
-                    <Col xs={5}>2 of 3 (wider)</Col>
+                    <Col>2 of 3</Col>
                     <Col>3 of 3</Col>
                 </Row>
-            </Container>
+        </Container>
         </div>
     )
 }
