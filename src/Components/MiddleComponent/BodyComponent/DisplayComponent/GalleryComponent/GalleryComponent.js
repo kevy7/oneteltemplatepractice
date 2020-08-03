73667b1cdd7c27ee84e42b9e33be03ecd26b7a3f
kevy7 <kvnyang503@gmail.com>
2020-08-02 18:31:46 -0700

    added more columns to Gallery Component

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -10,10 +10,12 @@ const GalleryComponent = () => {
                 <Row>
                     <Col>1 of 2</Col>
                     <Col>2 of 2</Col>
+                    <Col>2 of 2</Col>
                 </Row>
                 <Row>
                     <Col>1 of 3</Col>
                     <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
                     <Col>3 of 3</Col>
                 </Row>
         </Container>
