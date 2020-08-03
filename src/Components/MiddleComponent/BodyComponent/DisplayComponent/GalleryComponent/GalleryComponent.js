50844a1c4284ca8fde23cd9b518069b95c7e89f0
kevy7 <kvnyang503@gmail.com>
2020-08-02 20:06:14 -0700

    removed an import

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Row, Col} from 'react-bootstrap';
 import styles from './GalleryComponent.module.css';
-//import { Container, Row, Col} from 'react-bootstrap';
 
 const GalleryComponent = () => {
 
