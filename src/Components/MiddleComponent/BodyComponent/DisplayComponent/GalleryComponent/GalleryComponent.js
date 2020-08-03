000d8b44a05802fe29bc0bb93e6baa04464464e3
kevy7 <kvnyang503@gmail.com>
2020-08-02 19:45:19 -0700

    added unsplash images to page

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/GalleryComponent/GalleryComponent.js
@@ -9,15 +9,27 @@ const GalleryComponent = () => {
             <Container>
                 <Row>
                     <Col xs={6} sm={6} md={4}>
-                        <img src="https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Italian Trulli" />
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1593642532871-8b12e02d091c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
                     </Col>
-                    <Col xs={6} sm={6} md={4}>2 of 2</Col>
-                    <Col xs={6} sm={6} md={4}>2 of 2</Col>
+                    <Col xs={6} sm={6} md={4}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
+                    <Col xs={6} sm={6} md={4}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
 
-                    <Col xs={6} sm={6} md={3}>1 of 3</Col>
-                    <Col xs={6} sm={6} md={3}>2 of 3</Col>
-                    <Col xs={6} sm={6} md={3}>3 of 3</Col>
-                    <Col xs={12} sm={6} md={3}>3 of 3</Col>
+                    <Col xs={6} sm={6} md={3}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
+                    <Col xs={6} sm={6} md={3}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1483478550801-ceba5fe50e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
+                    <Col xs={6} sm={6} md={3}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
+                    <Col xs={12} sm={6} md={3}>
+                        <img className={styles.image} src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </Col>
                 </Row>
         </Container>
         </div>
