4a347ee909b867508a11e9c518c91b369ec2b801
kevy7 <kvnyang503@gmail.com>
2020-08-03 18:56:59 -0700

    added classname for paragraph

--- src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayInfoComp/DisplayInfoComp.js
+++ src/Components/MiddleComponent/BodyComponent/DisplayComponent/DisplayInfoComp/DisplayInfoComp.js
@@ -5,7 +5,7 @@ const DisplayInfoComp = () => {
     return (
         <div className={Styles.displayInfoComp}>
             <h1>Title</h1>
-            <p>
+            <p className={Styles.displayParagraph}>
                 sjsio sdhf dhsfdihs fdi flidhs gdhnl  sdfid lf hdslif hdsilh fldskng ldsng
                 njkdf s bnl jg sldkjn djgn n fglkfg fsglkfsng lknsjg lkfn gfg lkfn g sdfgjl glf 
                 fnlgn lf fs gnlksfg lfijng s glkfns glfsng lfkm gkfsn jglis gls gsjgn fgsngjl.
