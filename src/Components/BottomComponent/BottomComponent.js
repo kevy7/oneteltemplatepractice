801d5132af72613c4441f5123987661008d7f0f4
kevy7 <kvnyang503@gmail.com>
2020-08-04 21:08:35 -0700

    created all elements for the BottomComponent

--- src/Components/BottomComponent/BottomComponent.js
+++ src/Components/BottomComponent/BottomComponent.js
@@ -6,7 +6,21 @@ const BottomComponent = () => {
     return (
         <div className={Styles.bottomComponent}>
             <ContactInfo />
-            
+            <div className={Styles.linksElement}>
+                <h2>See Links</h2>
+                <p>Link 1</p>
+                <p>Link 2</p>
+                <p>Link 3</p>
+                <p>Link 4</p>
+            </div>
+            <div>
+                <h2>Email Us</h2>
+                <p>dksngfod ns fnkldsnf lsdnf aldf lsn flnf lsnd fksdn nsd lfds.</p>
+                <input type="text" id="email" name="email" placeholder="Email"/>
+                <div>
+                    <button className={Styles.bottomCompButton}>Enter</button>
+                </div>
+            </div>
         </div>
     )
 }
