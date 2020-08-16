02033d24b9f250b74efeb031108ead1cc64770ec
kevy7 <kvnyang503@gmail.com>
2020-08-16 00:20:23 -0700

    made minor changes to text

--- src/Components/BottomComponent/BottomComponent.js
+++ src/Components/BottomComponent/BottomComponent.js
@@ -15,7 +15,7 @@ const BottomComponent = () => {
             </div>
             <div className={Styles.emailComponent}>
                 <h3>Email Us</h3>
-                <p>dksngfod ns fnkldsnf lsdnf aldf lsn flnf lsnd fksdn nsd lfds dfhdf sdfjkdsjg fkasdlfk.</p>
+                <p>dksngfod ns fnkldsnf lsdnf aldf lsn flnf lsnd fksdn nsd lfds dfhdf sdfjkdsjg fkasdlfk dfh dkfd.</p>
                 <input className={Styles.input}type="text" id="email" name="email" placeholder="Email"/>
                 <div>
                     <button className={Styles.bottomCompButton}>Enter</button>
