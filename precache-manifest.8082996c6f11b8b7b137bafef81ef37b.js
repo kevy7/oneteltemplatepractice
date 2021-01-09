40547f6b47540c34c2219c300b836783689846f8
kevy7 <kvnyang503@gmail.com>
2021-01-08 17:07:08 -0800

    Updates

---
+++ precache-manifest.8082996c6f11b8b7b137bafef81ef37b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cbacaabe1bce05db4a9088a5ee8f06",
+    "url": "/oneteltemplatepractice/index.html"
+  },
+  {
+    "revision": "c237768f5e4708b0e3f3",
+    "url": "/oneteltemplatepractice/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f5707373a5aa2fc154a",
+    "url": "/oneteltemplatepractice/static/css/main.a72ff930.chunk.css"
+  },
+  {
+    "revision": "c237768f5e4708b0e3f3",
+    "url": "/oneteltemplatepractice/static/js/2.612a960c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/oneteltemplatepractice/static/js/2.612a960c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5707373a5aa2fc154a",
+    "url": "/oneteltemplatepractice/static/js/main.0b15134b.chunk.js"
+  },
+  {
+    "revision": "565f0577050296374643",
+    "url": "/oneteltemplatepractice/static/js/runtime-main.a8bc40d4.js"
+  }
+]);
